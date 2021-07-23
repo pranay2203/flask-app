5020fbf1bf7dfbbd08ae15af9c43758d681ee181
Pranay Shah <pranay2203@gmail.com>
2021-07-22 22:10:18 -0500

    update

--- website/auth.py
+++ website/auth.py
@@ -29,37 +29,16 @@ def login():
 
     return render_template("login.html", user=current_user)
 
-@auth.route('/login/<string:id>', methods=['GET', 'POST'])
-def login(id):
+#@auth.route('/login/<string:id>', methods=['GET', 'POST'])
+#def login(id):
 
-    try:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), id)
-        userid = idinfo['sub']
+    #try:
+        #idinfo = id_token.verify_oauth2_token(token, requests.Request(), id)
+        #userid = idinfo['sub']
 
-    except ValueError:
+    #except ValueError:
         # Invalid token
-        pass
-
-
-
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
-        else:
-            flash('Email does not exist.', category='error')
-
-
-
-    return render_template("login.html", user=current_user)
+        #pass
 
 @auth.route('/logout')
 @login_required
